2561563bff89094b0638f1a2efca9e4532d0a06c
Akavi <gabdushev.i@yandex.ru>
2022-05-14 10:57:54 +0500

    update

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,6 @@ export PATH="$PATH":"/home/akavi/.local/bin"
 export PATH="$PATH":"/home/akavi/flutter/bin"
 
 set -Ux FZF_DEFAULT_OPTS "--color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD"
-set -Ux EDITOR neovide
+set -Ux EDITOR nvim
 
 source (dirname (status -f))/aliases.fish
